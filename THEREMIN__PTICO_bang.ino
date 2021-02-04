8264640fa0db909b5a7ccdc88cad128b4ea8fa0c
chenbangwei <78345850+chenbangwei@users.noreply.github.com>
2021-02-04 10:57:55 +0100

    Add files via upload

---
+++ THEREMIN__PTICO_bang.ino
@@ -0,0 +1,48 @@
+int switchState =0;
+int buttonPin = 7;
+bool play = false;
+bool isTheButtonBeingPressed = false;
+int sensorValue =0;
+int sensorLow = 1023;
+int sensorHigh = 0;
+const int ledPin = 13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin,OUTPUT);
+digitalWrite(ledPin,HIGH);
+while(millis() < 5000) {
+  sensorValue = analogRead(A0);
+  if(sensorValue > sensorHigh) {
+    sensorHigh = sensorValue;
+  }if(sensorValue < sensorLow){
+    sensorLow = sensorValue;
+  }
+}
+digitalWrite(ledPin,LOW);
+}
+
+
+void loop() {
+checkButton();
+if (play) {
+  sound();
+}
+}
+
+void checkButton(){
+  switchState = digitalRead(buttonPin);
+  if(switchState == HIGH){
+    if(isTheButtonBeingPressed == false){
+      play = !play;
+      isTheButtonBeingPressed = true;
+    }
+}
+else{isTheButtonBeingPressed = false;
+}
+}
+void sound(){
+  sensorValue = analogRead(A0);
+  int pitch = map(sensorValue,sensorLow,sensorHigh,50,4000);
+  tone(8,pitch,20);
+  delay(10);
+}
